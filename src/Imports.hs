091ee65b01a8e27923a6a3f00f2e45adcb51055a
rvion <vion.remi@gmail.com>
2014-12-21 15:12:28 -0800

    tweaks

--- src/Imports.hs
+++ src/Imports.hs
@@ -3,7 +3,7 @@ module Imports
   , module X
   ) where
 
-import           Control.Lens              as X
+import           Control.Lens              as X hiding ((.=))
 
 import           Network.HTTP.Conduit      as X
 import           Network.HTTP.Types.Header as X
@@ -12,6 +12,8 @@ import           Network.HTTP.Types.Method as X
 import           Control.Applicative       as X ((<$>), (<*>))
 import           Control.Monad             as X (liftM)
 import           Data.Monoid               as X ((<>))
+
+import Data.Aeson as X
 
 mba ? def = case mba of
     Just a -> a
--- src/Test.hs
+++ src/Test.hs
@@ -1,13 +1,26 @@
 {-# LANGUAGE PackageImports #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Test where
-import "this" Imports
 
+import           API.Rest
+import           "this" Imports
+
+main :: IO ()
 main = do
+    debugRequest
     if all (== True) tests
     then print "all good"
     else print "error"
 
-tests = 
+tests :: [Bool]
+tests =
   [ Nothing ? 3 == 3
   , Just "d" ? "fre" == "d"
   ]
+
+debugRequest :: IO ()
+debugRequest = do
+    let fakeWebsite = apiWrapper "https://fakeWebsite.test"
+        reqBody = RequestBodyLBS $ encode $ object ["grant_type" .= ("client_credentials" :: String)]
+    fakeWebsite "/oauth2/token" methodPost (Just reqBody) (NoAuth)
+    return ()
